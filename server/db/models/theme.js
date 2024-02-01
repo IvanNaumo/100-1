e61f75cd3f4e050d37a113b18b95f9d1bce3406b
Sergey8888888 <sergeyoganov01@gmail.com>
2024-02-01 17:24:03 +0300

    first

---
+++ server/db/models/theme.js
@@ -0,0 +1,51 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Theme extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User, Question }) {
+      this.belongsTo(User, { foreignKey: 'user_id' });
+      this.hasMany(Question, { foreignKey: 'theme_id' });
+      // define association here
+    }
+  }
+  Theme.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      user_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Theme',
+    }
+  );
+  return Theme;
+};
---
+++ server/db/models/user.js
@@ -0,0 +1,54 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Answer }) {
+      this.hasMany(Answer, { foreignKey: 'user_id' });
+    }
+  }
+  User.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    email: {
+      unique: true,
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    password: {
+      unique: true,
+      allowNull: false,
+      type: DataTypes.TEXT,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
