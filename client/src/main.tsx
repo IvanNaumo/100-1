53a7b61ca0e66d5ff547c9e55c2f405234500126
Igor_L <igor101188@gmail.com>
2024-02-02 14:13:15 +0300

    FIX

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
+
 import App from './App/App';
-=======
-import App from './App';
->>>>>>> 19c193e72d41074438d872cb3a71bdee85957ba0
+
 import './index.css';
 import { store } from './redux/store';
 
@@ -16,12 +14,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <App />,
     </BrowserRouter>
-<<<<<<< HEAD
-    ,
   </Provider>,
-=======
-  </Provider>,
-
->>>>>>> 19c193e72d41074438d872cb3a71bdee85957ba0
   // </React.StrictMode>,
 );
